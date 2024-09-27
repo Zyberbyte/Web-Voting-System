709fc228abc013679724527d9707a5cc31b42df4
narra <ermanfaminiano020@gmail.com>
2024-09-27 14:55:38 +0800

    added git ignore

--- client/src/pages/Authentication/login.jsx
+++ client/src/pages/Authentication/login.jsx
@@ -5,16 +5,18 @@ import { Logo, ProfileIcon } from "../../assets/img/image";
 function login() {
   return (
     <>
-      <div className=" flex flex-col justify-center items-center h-[100vh]">
-        <div className="flex flex-col items-center justify-center w-[30%]">
-          <div className=" w-40 h-40 mb-5">
-            <img src={Logo} alt="logo" />
-          </div>
-          <div className="flex flex-col items-center justify-center border-2 border-gray-500 rounded-sm w-full h-full bg-gray-300 bg-opacity-10 backdrop-filter backdrop-blur-lg">
+      <div className=" flex flex-col justify-center items-center h-[100vh] bg-yellow-300">
+        <div className="flex flex-col items-center justify-center w-[30%] bg-white border rounded-md">
+          <div className="flex flex-col items-center justify-center border-2  w-full h-full bg-gray-300 ">
             <div className=" w-10 h-10">
+
               <img src={ProfileIcon} alt="" />
             </div>
           </div>
+          <div className=" w-40 h-40 mb-5">
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="p-8 w-full"></div>
         </div>
       </div>
     </>
